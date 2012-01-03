95cca97a36510fe99bac83e2b4dc7e18ff357aec
Berg Furniture <berg-usa@bergfurniture.com>
2012-01-03 10:48:27 -0500

    Reverted back to old logo swipe. Changed proportions of Chifferobe.

--- header.php
+++ header.php
@@ -3,36 +3,31 @@
 <div id="Header">
 	<!-- logo -->
 	<div id="hidden-logos">
-		<p>Looking for our logo for your own use? Please contact marketing@bergfurniture.com
+		<p>Gotcha! Were you trying to swipe our logo? <span id="smiley" class="rotate-90">:)</span> Why not grab a hi-res version instead?
 		<span>
-			<!--
-				
-				<a href="/img/Berg-Logo-Black-transparent.png">Black</a>
+			<a href="/img/Berg-Logo-Black-transparent.png">Black</a>
 
 			 -or-
 			
 			<a href="/img/Berg-Logo-White-transparent.png">White</a>.
 		</span>
-			
-		-->
-		
 		<a href='#' class="close">close</a>
 		</p>
 	</div> 
 	
 	<!-- Logo -->
 	
-	<h1><a href="index.php"><img title="Left Click Go Home" alt="Berg Furniture" src="img/logo.png"/></a></h1>
+	<h1><a href="/index.php"><img title="Left Click Go Home, Right Click for Logos" alt="Berg Furniture" src="/img/logo.png"/></a></h1>
 	<div class="hidden">Berg Furniture</div>
 
     <!-- Smart Bedroom Systems -->
 	<div id="SBSsmall">
-		<img alt="Smart Bedroom Systems, Innovative Environments for the Growing Child" src="img/SBSsmall.png" style= "position:absolute; margin-left:475px;"/>
+		<img alt="Smart Bedroom Systems, Innovative Environments for the Growing Child" src="/img/SBSsmall.png" style= "position:absolute; margin-left:475px;"/>
 	</div>
 
     <!-- Nav Bar -->
 	<div id="navigation2" class="editable">
-		<div id="home"><a href="index.php"></a></div>
+		<div id="home"><a href="/index.php"></a></div>
 			<ul id="menu">
 				<li class="mega">
 					<h2>
@@ -43,93 +38,93 @@
 
                 	<div style="border-right: #CCC 2px solid; float: left; width: 200px; padding-right: 40px; margin-top:10px; margin-bottom:10px;">
 
-                          <a href="space_savers.php"><img src="img/images/furn_icon_01.png" alt="Space Savers"/></a>
+                          <a href="/space_savers.php"><img src="/img/images/furn_icon_01.png" alt="Space Savers"/></a>
 
-						  <h3><a href="space_savers.php">Space Savers</a></h3>
+						  <h3><a href="/space_savers.php">Space Savers</a></h3>
 
 						  <p>
 
-							  <a href="22_805.php">22-805</a>,
+							  <a href="/22_805.php">22-805</a>,
 
-							  <a href="22_807.php">22-807</a>,
+							  <a href="/22_807.php">22-807</a>,
 
-							  <a href="22_808.php">22-808</a>,
+							  <a href="/22_808.php">22-808</a>,
 
-							  <a href="22_815.php">22-815</a>,
+							  <a href="/22_815.php">22-815</a>,
 
-							  <a href="22_816.php">22-816</a>,
+							  <a href="/22_816.php">22-816</a>,
 
-							  <a href="22_825.php">22-825</a>,
+							  <a href="/22_825.php">22-825</a>,
 
-							  <a href="22_827.php">22-827</a>
+							  <a href="/22_827.php">22-827</a>
 
 						  </p>
 
-						<a href="captains_bed.php"><img src="img/images/furn_icon_02.png" alt="Captain's Bed" style="margin-top:5px;" /></a>
+						<a href="/captains_bed.php"><img src="/img/images/furn_icon_02.png" alt="Captain's Bed" style="margin-top:5px;" /></a>
 
-						<h3><a href="captains_bed.php">Captain's Beds</a></h3>
+						<h3><a href="/captains_bed.php">Captain's Beds</a></h3>
 
                          <p>
 
-							 <a href="22_895.php">22-895</a>,
+							 <a href="/22_895.php">22-895</a>,
 
-							 <a href="22_905.php">22-905</a>,
+							 <a href="/22_905.php">22-905</a>,
 
-							 <a href="22_91.php">22-91</a>,
+							 <a href="/22_91.php">22-91</a>,
 
-							 <a href="22_915.php">22-915</a>,
+							 <a href="/22_915.php">22-915</a>,
 
-							 <a href="22_916.php">22-916</a>,
+							 <a href="/22_916.php">22-916</a>,
 
-							 <a href="22_931.php">22-931</a>,
+							 <a href="/22_931.php">22-931</a>,
 
-							 <a href="22_935.php">22-935</a>
+							 <a href="/22_935.php">22-935</a>
 
                          </p>
 
-                         <a href="jr_captains_bed.php"><img src="img/images/furn_icon_07.png" alt="Jr. Captain's Bed" style="margin:0px 25px 0 4px;"/></a>
+                         <a href="/jr_captains_bed.php"><img src="/img/images/furn_icon_07.png" alt="Jr. Captain's Bed" style="margin:0px 25px 0 4px;"/></a>
 
-                         <h3><a href="jr_captains_bed.php">Jr. Captain's Beds</a></h3>
+                         <h3><a href="/jr_captains_bed.php">Jr. Captain's Beds</a></h3>
 
                          <p>
 
-							 <a href="22_940.php">22-940</a>,
+							 <a href="/22_940.php">22-940</a>,
 
-							 <a href="22_950.php">22-950</a>,
+							 <a href="/22_950.php">22-950</a>,
 
-							 <a href="22_952.php">22-952</a>,
+							 <a href="/22_952.php">22-952</a>,
 
-							 <a href="22_959.php">22-959</a>,
+							 <a href="/22_959.php">22-959</a>,
 
-							 <a href="22_965.php">22-965</a>
+							 <a href="/22_965.php">22-965</a>
 
                          </p>
 
-                         <a href="22_68.php"><img src="img/images/furn_icon_10.png" alt="Platform Beds" style="margin-top:-8px; margin-right:20px;"/></a>
+                         <a href="/22_68.php"><img src="/img/images/furn_icon_10.png" alt="Platform Beds" style="margin-top:-8px; margin-right:20px;"/></a>
 
-                         <h3><a href="platform_beds.php">Platform Beds</a></h3>
+                         <h3><a href="/platform_beds.php">Platform Beds</a></h3>
 
                          <p>
 
-							 <a href="22_68.php">22-68 Twin</a>,
+							 <a href="/22_68.php">22-68 Twin</a>,
 
-							 <a href="22_87.php">22-87 Full</a>
+							 <a href="/22_87.php">22-87 Full</a>
 
                           <br />
 
                         </p>
 
-						<a href="enterprise_collection.php"><img src="img/images/furn_icon_03.png" style="margin-top:10px;" alt="Enterprise"/></a>
+						<a href="/enterprise_collection.php"><img src="/img/images/furn_icon_03.png" style="margin-top:10px;" alt="Enterprise"/></a>
 
-                         <h3><a style="padding-top:10px;" href="enterprise_collection.php">Enterprise</a></h3>
+                         <h3><a style="padding-top:10px;" href="/enterprise_collection.php">Enterprise</a></h3>
 
                          <p>
 
-							 <a href="40_415.php">40-415</a>,
+							 <a href="/40_415.php">40-415</a>,
 
-							 <a href="40_515.php">40-515</a>,
+							 <a href="/40_515.php">40-515</a>,
 
-							 <a href="40_525.php">40-525</a>,
+							 <a href="/40_525.php">40-525</a>,
 
 
                          </p>
@@ -140,33 +135,33 @@
 
                    <div style="float:left; padding-left: 40px; width: 200px; margin-top:10px;"><!-- Second Column -->
 
-                         <a href="utica.php"><img src="img/images/furn_icon_04.png" alt="Utica" style="width:60px;"/></a>
+                         <a href="/utica.php"><img src="/img/images/furn_icon_04.png" alt="Utica" style="width:60px;"/></a>
 
-                         <h3><a href="utica.php">Utica</a></h3>
+                         <h3><a href="/utica.php">Utica</a></h3>
 
                          <p>
 
-							 <a href="23_805.php">23-805</a>,
+							 <a href="/23_805.php">23-805</a>,
 
-							 <a href="23_815.php">23-815</a>,
+							 <a href="/23_815.php">23-815</a>,
 
-							 <a href="23_835.php">23-835</a>,
+							 <a href="/23_835.php">23-835</a>,
 
-							 <a href="23_905.php">23-905</a>,
+							 <a href="/23_905.php">23-905</a>,
 
-							 <a href="23_777.php">23-777</a>,
+							 <a href="/23_777.php">23-777</a>,
 
-							 <br /><a href="mix_and_match.php">Mix &amp; Match</a>
+							 <br /><a href="/mix_and_match.php">Mix &amp; Match</a>
 
                          </p>
 
-						 <a href="play_and_study.php"><img src="img/images/furn_icon_05.png" alt="Play and Study" style="margin-top:-5px; width:60px; padding-bottom: 7px;" /></a>
+						 <a href="/play_and_study.php"><img src="/img/images/furn_icon_05.png" alt="Play and Study" style="margin-top:-5px; width:60px; padding-bottom: 7px;" /></a>
 
-                         <h3><a href="play_and_study.php">Play &amp; Study</a></h3>
+                         <h3><a href="/play_and_study.php">Play &amp; Study</a></h3>
 
 							 <p>
 
-								 <a href="91_74.php">91-74</a>
+								 <a href="/91_74.php">91-74</a>
 
 							</p> 
 
@@ -175,29 +170,29 @@
 
                         <!---  Case Goods-->
 
-						<a href="case_goods.php"><img src="img/images/furn_icon_11.png" alt="Case Goods" style="margin-top:8px; margin-right:29px; margin-bottom:40px;"/></a>
+						<a href="/case_goods.php"><img src="/img/images/furn_icon_11.png" alt="Case Goods" style="margin-top:8px; margin-right:29px; margin-bottom:40px;"/></a>
 
-                         <h3><a href="case_goods.php">Case Goods</a></h3>
+                         <h3><a href="/case_goods.php">Case Goods</a></h3>
 
                          <p>
 
-								<a href="case_goods.php#row1">Dressers &amp; Chests</a>,
+								<a href="/case_goods.php#row1">Dressers &amp; Chests</a>,
 
-								<a href="case_goods.php#row2">Night Tables</a>,
+								<a href="/case_goods.php#row2">Night Tables</a>,
 
-								<a href="case_goods.php#row2">Mirror</a>,
+								<a href="/case_goods.php#row2">Mirror</a>,
 
-								<a href="case_goods.php#row2">Deluxe Chair</a>,
+								<a href="/case_goods.php#row2">Deluxe Chair</a>,
 
-								<a href="case_goods.php#row3">Desks &amp Accessories</a>,
+								<a href="/case_goods.php#row3">Desks &amp Accessories</a>,
 
-								<a href="case_goods.php#row4">Bookcases</a>,
+								<a href="/case_goods.php#row4">Bookcases</a>,
 
-								<a href="case_goods.php#row5">Trundle</a>,
+								<a href="/case_goods.php#row5">Trundle</a>,
 
-								<a href="case_goods.php#row5">Under-bed Drawers</a>,
+								<a href="/case_goods.php#row5">Under-bed Drawers</a>,
 
-								<a href="case_goods.php#row6">Shelves</a>
+								<a href="/case_goods.php#row6">Shelves</a>
 
                          </p>
 
@@ -207,17 +202,17 @@
 
 						<div id="menuBottom"> <!-- 'Footer' of menu-->
 
-						<a href="discontinued.php"><img src="img/images/73-424.png" alt="discontinued" style="width:50px;margin-top:-5px; margin-right:29px; margin-bottom:40px;"/></a>
+						<a href="/discontinued.php"><img src="/img/images/73-424.png" alt="discontinued" style="width:50px;margin-top:-5px; margin-right:29px; margin-bottom:40px;"/></a>
 
-						<h3><a style="color:grey" href="discontinued.php">Discontinued Items</a></h3> 
+						<h3><a style="color:grey" href="/discontinued.php">Discontinued Items</a></h3> 
 
 							<p>
 
-								<a style="color:grey" href="discontinued.php#cribsanchor">Nursery</a><br />
+								<a style="color:grey" href="/discontinued.php#cribsanchor">Nursery</a><br />
 
-								<a style="color:grey" href="discontinued.php#osloanchor">Oslo Collection</a><br />
+								<a style="color:grey" href="/discontinued.php#osloanchor">Oslo Collection</a><br />
 
-								<a style="color:grey" href="discontinued.php#otherModels">Other</a>
+								<a style="color:grey" href="/discontinued.php#otherModels">Other</a>
 
 							</p>
 
