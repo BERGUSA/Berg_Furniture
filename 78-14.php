6bcf333e65cc2c47ea6e690dc6f72e0419dd7e53
Berg Furniture <berg-usa@bergfurniture.com>
2012-01-05 11:52:44 -0500

    Added transforming bed page Added * on options page

---
+++ 78-14.php
@@ -0,0 +1,136 @@
+<?php include 'head.php'; ?>
+<title>Berg Furniture | Jr. Captain's Bed | 22&ndash;950</title>
+
+</head>
+	<body>
+    	<div id="outside_container">
+
+			<?php include 'options.php'; ?>
+
+            <div id="tip"></div>
+            <div class="container">
+                
+                <?php include 'header.php'; ?>
+                
+               <div class="prodContainer"> 
+               		<div class="prodTitle">
+                    	<p><a href="jr_captains_bed.php">The Jr. Captain's Bed Collection</a></p>
+                    </div>
+                 <div class="prodMain">
+                        <div class="prodCont"><style> .prodTitle p a:hover {color:#8be93b ;}</style>
+                        		<h2>Low Frame Twin</h2><h3>#22-950</h3>
+                                <br />
+                                <p>This great low-frame Captain's Bed is available in twin and full sizes. The low-set frame and sleek, clean arrangement of storage space makes it perfect for teens and young adults.  The 8 extra-deep drawers and additional shelves provide plenty of organized space, without sacrificing style.  <br /><br /><b>Optional Bookcase Shown (#22-95A)</b>
+                                </p>
+                                <br />
+                                <h3>Features</h3>
+                                <ul>
+                                	<li>Twin Size Bed</li>
+                                    <li>8 Drawers</li>
+                                    <li>Side Shelves</li>
+                                    
+                                </ul>
+                                <div class="dimensions"><img src="img/dimbkg.png"/>
+                                    <div style="top:21px; left: 5px;">72"H</div>
+                                    <div style="top:40px; left: 40px;">44"D</div>
+                                    <div style="top:77px; left: 70px;">83"L</div>
+                                </div>
+                                <br />
+                               <a href="#" rel="#knobs">Click for Knob Options&nbsp;&gt;&gt;</a><br />
+                                <a href="#" rel="#finishes">Click for Finish Options&nbsp;&gt;&gt;</a></br >
+								<a href="#" rel="#accents">Click for Accent Options&nbsp;&gt;&gt;</a>
+                        </div>
+                   <div class="prodShow">
+                   	 <div class="imgWrap">
+                            	<img src="img/1px.gif" width="500" height="392" />
+                                <div id="image_caption"></div>
+                            </div>
+                            <a class="prevPage browse left"></a>
+                            	<div class="scrollable">
+                                	<div class="items">
+                                    	<img src="img/MainProdPics/22-950A_t.jpg" 
+                                        title="Shown in Brandy; <b><i>Optional</b> Bookcase and Custom Knobs*</i>"/>
+                                        
+                                        <img src="img/MainProdPics/22-950B_t.jpg" 
+                                        title="Shown in White; <b><i>Optional</b> Lime Green Oslo Knobs and Bookcase</i>"/>
+                                        
+                                        <img src="img/MainProdPics/22-950C_t.jpg" 
+                                        title="Shown in Natural; <b><i>Optional</b> Red Oslo Knobs</i>"/>
+                                      
+                                       
+                                       
+                                    </div>
+                                </div>
+                            <a class="nextPage browse right"></a>
+                     </div>
+                         
+                      <div id="iconBar">
+                            
+                         <div id="icons">
+                           	 <img src="img/images/icons/1.png" 
+                                	title="<h4>One Bed</h4><br />This Smart Bedroom System is designed to provide an enriching environment for one person" />
+                              
+                             
+                             
+                             <img src="img/images/icons/twin.png" 
+                                title="<h4>Twin Bed</h4><br />The frame on this furniture set is built to support a twin 	
+                                			 sized bed. Mattress not included." />
+                                			 
+                             
+                              <img src="img/images/icons/twin.png" 
+                                title="<h4>Twin Bed</h4><br />The frame on this furniture set is built to support a twin 	
+                                			 sized bed. Mattress not included." />  			 
+                                			 
+                               <img src="img/images/icons/twin.png" 
+                                title="<h4>Twin Bed</h4><br />The frame on this furniture set is built to support a twin 	
+                                			 sized bed. Mattress not included." /> 			 
+                                			 
+                                			 
+                                			 
+                                			 
+                                			 
+                                			 
+                                             <div id="rollover">Roll-over icons for more info</div>
+                            
+                             
+                         </div>
+                     </div>        
+                   </div>
+                    <div class="prodFtr">
+                    	<style> #thisColl a:hover {color:#8be93b;}</style>
+                        <div id="thisColl"><b><a href="/jr_captains_bed.php">All Jr. Captain's Beds</a></b>
+                        		<br /><br />
+                              <ul>
+                               	  <li><a href="22_940.php"><b>22-940</b> High Frame Twin</a></li>
+                                  <li style="display:block; background-color: #8be93b;color:#000;"><b>22-950</b> Low Frame Twin</li>
+                                  <li><a href="22_952.php"><b>22-952</b> Low Frame Full 16 Drawer</a></li>
+                                  <li><a href="22_959.php"><b>22-959</b> Low Frame Full</a></li>
+                                  <li><a href="22_965.php"><b>22-965</b> High Frame Twin w/Stairs</a></li>
+                                  
+                                  <br />
+                              </ul>
+                        </div>
+                        <div id="similarItems"><b>You Might Also Like:</b>
+                        		<br /><br />
+                              <ul>
+                                  <li><a href="22_940.php"><b>22-940</b> Jr. Captain's Twin</a></li>
+                                  <li><a href="22_895.php"><b>22-895</b> Captain's Bed Twin</a></li>
+                                  <li><a href="22_931.php"><b>22-931</b> Captain's Bed Full</a></li>
+                                  <li><a href="91_74.php"><b>91-74</b> Play &amp; Study</a></li>
+                                  <li><a href="40_830.php"><b>40-830</b> Enterprise Twin</a></li>
+                                  
+                              </ul>
+                      </div>
+                    
+                    <?php include 'CGs_links.php'; ?>
+                    
+                 </div>
+               </div>             
+            </div>  
+       </div>
+   	
+    <?php include 'footer.php'; ?> 
+    
+</body> 
+</html>        
+          																																																																				
--- options.php
+++ options.php
@@ -4,8 +4,14 @@
                 </div>
               <div class="simple_overlay" id="finishes">
                   <img src="img/availfinishes.png" />
+                  <div class="simple_overlay_text" id="finishes">
+                  	*Also available as accents.
+                  </div>
                 </div>
 				<div class="simple_overlay" id="accents">
                 	<img src="img/availaccents.png" />
+                	<div class="simple_overlay_text" id="accents">
+                	*Finishes are also available as accents.
+                	</div>
                 </div>
 <!--End Knob, Finish, and Accent options-->
