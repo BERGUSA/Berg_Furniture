c7e40ead7f5c009d11e24cd41608d85992d50635
Berg Furniture <berg-usa@bergfurniture.com>
2012-05-14 16:41:17 -0400

    Start of Pet Website!

---
+++ head_pet.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html" charset="utf-8" />
+<meta name="description" content="Berg Furniture now offers a wide variety of pet furniture. Our items are fresh, unique, colorful, and exciting." />
+<meta name="keywords" content="Berg, Furniture, Pet, Beds" />
+
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+<!-- LINKS -->
+<link href="css/style_pet.css" rel="stylesheet" type="text/css" />
+<link href="http://fonts.googleapis.com/css?family=Lobster" rel="stylesheet" type="text/css">
+
+<!-- FAVICON -->
+<link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon" />
+<link rel="icon" href="img/favicon.ico" type="image/x-icon" />
+
+<!--[if lt IE 7]>
+<script type="text/javascript" src="/js/unitpngfix.js"></script>
+<![endif]--> 
---
+++ pets.php
@@ -0,0 +1,44 @@
+<?php include 'head_pet.php'; ?>
+<title>Berg Furniture | Pet Beds</title>
+
+</head>
+
+<body>
+
+<div class="container">
+	<!---HEADER--->
+	<div id="container" class="header">
+		
+		<div class="logo">
+			
+			<img src="img/pet/logo.png">
+			
+		</div>
+		
+		<div class="tagline">
+			
+			<img src="img/pet/pets_are_special.png">
+			
+		</div>
+		
+	</div>
+	
+	
+	
+	
+	<!---BODY--->
+	<div id="container" id="class">
+		
+		
+		
+	</div>
+	
+	</body>
+	
+	<!--------JS------>
+	
+	<script type="text/javascript" src="js/jquery-1.6.4.js"></script>
+    <script type="text/javascript" src="js/plugins.js"></script>
+	<script type="text/javascript" src="js/scripts.js"></script>
+	
+</div>
