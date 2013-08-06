d95f2561d90f164f0080f2d92e819b1a1afeee69
Berg Furniture <berg-usa@bergfurniture.com>
2013-08-06 11:14:20 -0400

    Switched out footer on Utica YouTube video.
    Switched out embedded link-- previous one wasn't working.

--- YouTubeVideo3.php
+++ YouTubeVideo3.php
@@ -12,54 +12,46 @@
                 
                <div class="prodContainer"> 
                		<div class="prodTitle">
-                    	<p><a href="78_12.php">The Twin XL/Twin Transforming Bed </a></p>
+                    	<p><a href="78_12.php">Our Versatile Utica Line</a></p>
                     </div>
                  <div class="prodMain">
                         
+                        <br><br><br>
                         
                         
-                        
-                        <iframe width="853" height="480" src="http://www.youtube.com/embed/WapX_ZGx5ZU?rel=0" frameborder="0" allowfullscreen></iframe>
-                        
-                        
+							<iframe width="640" height="360" src="//www.youtube.com/embed/WapX_ZGx5ZU?rel=0" frameborder="0" allowfullscreen></iframe>                        
                         
                          
                    </div>
                     <div class="prodFtr">
-                    	<style> #thisColl a:hover {color:#0b90a3;}</style>
-                        <div id="thisColl"><b><a href="/transforming_beds.php">All Transforming Beds</a></b>
-                        		<br /><br />
+                    	<style> #thisColl a:hover {color:#F60;}</style>
+                        <div id="thisColl"><b><a href="/utica.php">All Uticas</a></b>
+                        		<br /><br /> 
                               <ul>
-                          		  <li><a href="78_14.php"><b>78-14</b> Desk with Twin Bed Transforming Unit</a></li>
-                          		                            		 
-                                  <li style="display:block; background-color: #0b90a3;color:#000;"><a href="78_12.php"><b>78-12</b> Twin XL over Twin with Desk Transforming Unit</a></li>
-                              
-                              <li><!--<a href="22_959.php">--><b>COMING SOON!</b> Full Transforming System</a></li>
-                                  <br/>        
+                                  <li><a href="23_805.php"><b>23-805</b> Twin/Twin Loft</a></li>
+                                  <li><a href="23_815.php"><b>23-815</b> Twin/Full Loft</a></li>
+                                  <li><a href="23_835.php"><b>23-835</b> Twin Dorm Loft</a></li>
+                                  <li><a href="23_905.php"><b>23-905</b> Full Loft</a></li>
+                                  <li><a href="23_935.php"><b>23-935</b> Full/Full Dorm Loft</a></li>
+                                  <li><a href="23_766.php"><b>23-766</b> Bifecta Double Twin Loft</a></li>
+                                  <li style="display:block; background-color:#F60;"><b>23-777</b> Trifecta Triple Twin Loft</li>
+                                  <li><a href="mix_and_match.php"><b>Mix &amp; Match</b></a></li>
                                   <br />
+                                  
                               </ul>
                         </div>
-                     <div id="similarItems"><b>You Might Also Like:</b>
+                        <div id="similarItems"><b>You Might Also Like:</b>
                         		<br /><br />
                               <ul>
-                              	
-                              	<br />
-                              	<br />
-                              	<br />
-                              	<br />
-                              	<br />
-                              	
-                       <!--       <li><a href="22_940.php"><b>22-940</b> Jr. Captain's Twin</a></li>
-                                  <li><a href="22_895.php"><b>22-895</b> Captain's Bed Twin</a></li>
-                                  <li><a href="22_931.php"><b>22-931</b> Captain's Bed Full</a></li>
-                                  <li><a href="91_74.php"><b>91-74</b> Play &amp; Study</a></li>
-                                  <li><a href="40_830.php"><b>40-830</b> Enterprise Twin</a></li>
-                       -->       
+                                  <li><a href="22_805.php"><b>22-805</b> Space Saver Twin/Twin </a></li>
+                                  <li><a href="22_815.php"><b>22-815</b> Space Saver Twin/Full</a></li>
+                                  <li><a href="22_905.php"><b>22-905</b> Captain's Bed Twin/Twin</a></li>
+                                  
+                                  <li><a href="40_525.php"><b>40-525</b> Enterprise Twin/Full w/Desk</a></li>
                               </ul>
                       </div>
-                    
-                    <?php include 'CGs_links.php'; ?>
-                    
+                     
+                     <?php include 'CGs_links.php'; ?>
                  </div>
                </div>             
             </div>  
